02a0d6af17f2f7bd0c664d48d8ada6bc32d4ffa1
Camilo Agudelo Tapias <agudelo.tapias@gmail.com>
2023-10-31 14:00:06 -0500

    first commit-2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const storesAll = document.querySelectorAll(".store-girbaud-one");
     const imagenSede = document.querySelector(".imagen-sedes");
+    const filter = document.querySelector("#filtro");
 
     storesAll.forEach(store => {
         store.addEventListener("click", () => {
@@ -22,7 +23,55 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     });
+
+
+    /*****filtro */
+    filter.addEventListener("change", () => {
+        const citySelect = filter.value;
+        let cityLength = 0;
+        let fisrtStore = null;
+        
+
+        storesAll.forEach(store => {
+            
+            if(citySelect === "todos") {
+                store.style.display = "block"
+                cityLength ++;
+                if(cityLength === 1) {
+                    fisrtStore = store
+                }
+            }else {
+                const storeCity = store.getAttribute("data-city");
+                
+                if(citySelect === storeCity) {
+                    store.style.display = "block";
+                    cityLength++
+                    if(!fisrtStore) {
+                        fisrtStore = store;
+                        store.classList.add("active");
+
+
+                        const storesGirbaudImg = document.querySelector(".stores-girbaud-img img");
+                        if(store.id == 1) {
+                            storesGirbaudImg .src = "./assets/oviedo.webp"
+                        }else if(store.id == 2) {
+                            storesGirbaudImg .src = "./assets/multiplaza.webp"
+                        }
+                        else if(store.id == 3) {
+                            storesGirbaudImg .src = "./assets/MFG-Fabricato.webp"
+                        }
+                        else if(store.id == 4) {
+                            storesGirbaudImg .src = "./assets/bucaramanga.webp"
+                        }
+                    }
+                }else {
+                    store.style.display = "none";
+                    store.classList.remove("active");
+                }
+                
+            }
+        })
+        
+    })
 });
-
-
 
